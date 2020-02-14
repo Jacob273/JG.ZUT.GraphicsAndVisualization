16f6d7d9dc87a9c95816fd14b75ddfae0a1b47f9
Jacob273 <jakubgmur@op.pl>
2020-02-14 01:03:38 +0100

    Create RotateHead.cs

---
+++ Moving characters - Game/Unity scripts/RotateHead.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public class RotateHead : MonoBehaviour
+    {
+        private float rotationCounter = 38;
+        Pressed currentState = Pressed.Noone;
+
+
+        public enum Pressed
+        {
+            Noone,
+            UpPressed,
+            UpReleased,
+            DownPressed,
+            DownReleased,
+        }
+
+
+        void Update()
+        {
+            //Check the current state...
+            GetCurrentState();
+
+            switch (currentState)
+            {
+                case Pressed.DownPressed:
+                    transform.localRotation = Quaternion.Euler(new Vector3(rotationCounter--, transform.localRotation.y, transform.localRotation.z));
+                    break;
+
+                case Pressed.DownReleased:
+                case Pressed.UpReleased:
+                    transform.localRotation = Quaternion.Euler(new Vector3(rotationCounter, transform.localRotation.y, transform.localRotation.z));
+                    break;
+
+                case Pressed.UpPressed:
+                    transform.localRotation = Quaternion.Euler(new Vector3(rotationCounter++, transform.localRotation.y, transform.localRotation.z));
+                    break;
+
+                case Pressed.Noone:
+                    Debug.Log("rotationCounter::" + rotationCounter);
+                    break;
+            }
+
+        }
+
+        private void GetCurrentState()
+        {
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                currentState = Pressed.UpReleased;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                currentState = Pressed.UpPressed;
+            }
+
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                currentState = Pressed.DownReleased;
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                currentState = Pressed.DownPressed;
+            }
+        }
+    }
+}
