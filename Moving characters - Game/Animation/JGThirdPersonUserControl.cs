582206c3d769bf02b456eca0726b51f640a9030d
Jakub Gmur <jakubgmur@Op.pl>
2020-02-16 22:15:01 +0100

    1. Added animations + models from unity standard assets. 2. Added JGThirdPersonCharacter which is almost exact as ThirdPersonCharacter 3. Custom implementation of JGThirdPersonUserControl.cs

---
+++ Moving characters - Game/Animation/JGThirdPersonUserControl.cs
@@ -0,0 +1,113 @@
+using Assets.JakubGmur.Scripts;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace JakubGmur.Animations
+{
+    [RequireComponent(typeof(JGThirdPersonCharacter))]
+    public class JGThirdPersonUserControl : MonoBehaviour
+    {
+        /// <summary>
+        /// Character that we control
+        /// </summary>
+        private JGThirdPersonCharacter m_Character;
+
+        /// <summary>
+        /// Object that has PointsLoader component.
+        /// </summary>
+        public GameObject ownerOfPointsLoader;
+
+        /// <summary>
+        /// Limit anglee which has impact movement (if we strictly move or stop and rotate)
+        /// </summary>
+        public float limitAngle = 60;
+
+        /// <summary>
+        /// Local poinstLoader.
+        /// </summary>
+        private GameObjectPointsLoader pointsLoader;
+
+        /// <summary>
+        /// List of the target point character should visit.
+        /// </summary>
+        private LinkedList<Vector3> targetPoints;
+
+        /// <summary>
+        /// Current target point.
+        /// </summary>
+        private LinkedListNode<Vector3> currentTarget;
+
+        public float movementSpeed = 0.5f;
+        public float rotationSpeed = 0.5f;
+        public bool shouldCrouch = false;
+        public bool shouldJump = false;
+        public bool infiniteWalking = true;
+
+        private void Start()
+        {
+            targetPoints = new LinkedList<Vector3>();
+            m_Character = GetComponent<JGThirdPersonCharacter>();
+            pointsLoader = ownerOfPointsLoader.GetComponent<GameObjectPointsLoader>();
+
+            if (pointsLoader == null)
+            {
+                Debug.Log($"Could not load GameObjectPointsLoader!!!!!");
+                return;
+            }
+
+            foreach (var point in ownerOfPointsLoader.GetComponent<GameObjectPointsLoader>().GetPoints())
+            {
+                targetPoints.AddLast(point);
+            }
+
+            currentTarget = targetPoints.First;
+        }
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+            if (currentTarget != null)
+            {
+                Vector3 targetDirection = (currentTarget.Value - MyPosition).normalized * movementSpeed;
+                var distanceToTarget = Vector3.Distance(m_Character.transform.position, currentTarget.Value);
+                var angleBetweenMeAndTarget = Vector3.Angle(targetDirection, m_Character.transform.forward);
+
+                //duza odleglosc && maly kat = poruszamy sie!
+                if (distanceToTarget > 0.5f && angleBetweenMeAndTarget <= limitAngle)
+                {
+                    m_Character.Move(targetDirection, shouldCrouch, shouldJump);
+                }//duza odleglosc && duzy kat  = obracamy sie!
+                else if(angleBetweenMeAndTarget >= limitAngle)
+                {
+                    m_Character.Move(new Vector3(0, 0, 0), shouldCrouch, shouldJump, false);
+                    float singleStep = rotationSpeed * Time.deltaTime;
+                    Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+                    transform.rotation = Quaternion.LookRotation(newDirection);
+                }//mala odleglosc && maly kat = bierzemy nastepnego targeta i poruszamy sie!
+                else 
+                {
+                    //m_Character.Move(targetDirection, shouldCrouch, shouldJump);
+                    currentTarget = currentTarget.Next ?? currentTarget.List.First;
+                }
+
+                if (currentTarget.Next == null && !infiniteWalking)
+                {
+                    currentTarget = null;
+                    m_Character.Move(new Vector3(0, 0, 0), shouldCrouch, shouldJump);
+                    return;
+                }
+
+            }
+        }
+
+        public Vector3 MyPosition
+        {
+            get
+            {
+                return m_Character.transform.position;
+            }
+        }
+    }
+}
