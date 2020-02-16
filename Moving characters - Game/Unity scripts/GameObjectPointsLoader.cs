733482c47296249a4ce2ece733dabdc9a82f45ed
Jakub Gmur <jakubgmur@Op.pl>
2020-02-16 01:09:55 +0100

    1. Added IPointsLoader interface that IS USED by a PointsDrawer. 2. Added concrete IPointsLoader implementation => GameObjectsPointsLoader - it does supply poinst based on GameObjects positions.
    There was a requirement that getting points to be drawn may have different sources, all we need now is to implement IPointsLoader interface and GetPoints method

---
+++ Moving characters - Game/Unity scripts/GameObjectPointsLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public class GameObjectPointsLoader : MonoBehaviour, IPointsLoader
+    {
+        public List<GameObject> points;
+
+        public List<Vector3> GetPoints()
+        {
+            return points.Select(item => new Vector3()
+            {
+                x = item.transform.position.x,
+                y = item.transform.position.y,
+                z = item.transform.position.z,
+            }).ToList();
+        }
+    }
+}
---
+++ Moving characters - Game/Unity scripts/IPointsLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public interface IPointsLoader
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        List<Vector3> GetPoints();
+    }
+}
