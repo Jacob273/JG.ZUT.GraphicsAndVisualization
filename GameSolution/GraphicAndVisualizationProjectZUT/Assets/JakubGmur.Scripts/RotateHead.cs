e4fb7fbe087b368d1b1a97af94181dd565a8ff92
jacob273 <jakubgmur@op.pl>
2020-10-18 11:30:40 +0200

    removed unused log

--- GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/RotateHead.cs
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/RotateHead.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.JakubGmur.Scripts
 {
@@ -44,7 +39,7 @@ namespace Assets.JakubGmur.Scripts
                     break;
 
                 case Pressed.Noone:
-                    Debug.Log("rotationCounter::" + rotationCounter);
+                    //Debug.Log("rotationCounter::" + rotationCounter);
                     break;
             }
 
