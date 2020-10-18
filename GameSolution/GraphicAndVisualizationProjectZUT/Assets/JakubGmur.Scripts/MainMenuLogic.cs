86cf893d49c3f1e436a071ba83817654f53c6eeb
jacob273 <jakubgmur@op.pl>
2020-10-18 23:45:01 +0200

    added background + MainMenuLogic script

---
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/MainMenuLogic.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject AboutProgramMenu;
+
+    public string SceneToLoad;
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(SceneToLoad).completed += OnLoadNewScene;
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnLoadNewScene(AsyncOperation obj)
+    {
+        SceneManager.LoadSceneAsync(SceneToLoad).completed -= OnLoadNewScene;
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    public void ShowAboutProgramMenu()
+    {
+        MainMenu.SetActive(false);
+        AboutProgramMenu.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        MainMenu.SetActive(true);
+        AboutProgramMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
