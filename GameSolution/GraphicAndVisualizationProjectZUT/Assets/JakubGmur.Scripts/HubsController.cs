9c3f6e8dc609517e3bc6aea904e9fb4c85bd4077
jacob273 <jakubgmur@op.pl>
2020-10-19 20:40:25 +0200

    added HubsController whioch does turn on and turn off images

---
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/HubsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public class HubsController : MonoBehaviour
+    {
+        public List<GameObject> images;
+        public GameObject defaultImage;
+        
+        private GameObject actualImage;
+
+        public void Start()
+        {
+            foreach(var image in images)
+            {
+                image.SetActive(false);
+            }
+
+            if(defaultImage != null)
+            {
+                actualImage = defaultImage;
+            }
+            else
+            {
+                actualImage = images.FirstOrDefault();
+            }
+
+            actualImage.SetActive(true);
+        }
+
+        public void Update()
+        {
+            //TODO
+        }
+
+    }
+}
