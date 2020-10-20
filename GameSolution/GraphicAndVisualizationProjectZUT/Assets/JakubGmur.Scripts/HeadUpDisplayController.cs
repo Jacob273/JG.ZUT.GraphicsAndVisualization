1e698e636fbe1d7ff8154966e3380910811545a7
jacob273 <jakubgmur@op.pl>
2020-10-20 22:24:58 +0200

    HubsController was renamed to HeadUpDisplayController

--- GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/HeadUpDisplayController.cs
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/HeadUpDisplayController.cs
@@ -1,30 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.JakubGmur.Scripts
 {
-    public class HubsController : MonoBehaviour
+    public class HeadUpDisplayController : MonoBehaviour
     {
         public List<GameObject> images;
         public GameObject defaultImage;
-        
         private GameObject actualImage;
 
-
-        private const float offsetX = 520.0f;
-        private const float offsetY = -285.0f;
-        private const float offsetZ = 140.0f;
+        public void UpdateActualImage(GameObject image)
+        {
+            try
+            {
+                actualImage = image;
+            }
+            catch (Exception ex)
+            {
+                Debug.Log($"Could not set actual image. {ex.Message}");
+                actualImage = defaultImage;
+            }
+        }
 
         public void Start()
         {
-            foreach(var image in images)
+            foreach (var image in images)
             {
                 image.SetActive(false);
             }
 
-            if(defaultImage != null)
+            if (defaultImage != null)
             {
                 actualImage = defaultImage;
             }
@@ -35,14 +43,11 @@ namespace Assets.JakubGmur.Scripts
 
             actualImage.SetActive(true);
 
-
             var children = actualImage.GetComponentInChildren<Image>();
             var rect = children.GetComponentInChildren<RectTransform>();
 
             rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 40, rect.rect.width);
             rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 40, rect.rect.height);
-            Debug.Log(rect.rect.width);
-            Debug.Log(rect.rect.height);
         }
 
         public void Update()
