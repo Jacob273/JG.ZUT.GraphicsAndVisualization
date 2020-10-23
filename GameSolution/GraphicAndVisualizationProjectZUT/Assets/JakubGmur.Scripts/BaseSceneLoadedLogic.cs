d3f50854ce8b10b9e9cd0826daf9caf7a5627ad5
jacob273 <jakubgmur@op.pl>
2020-10-24 01:17:34 +0200

    added ObjectsDeactivactorScript

---
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/BaseSceneLoadedLogic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public abstract class BaseSceneLoadedLogic : MonoBehaviour
+    {
+        public List<GameObject> objects;
+
+        public abstract void ExecuteLogic(GameObject obj);
+
+        public void ExecuteForAll()
+        {
+            if (objects != null)
+            {
+                foreach (var obj in objects)
+                {
+                    ExecuteLogic(obj);
+                }
+            }
+        }
+
+        void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            ExecuteForAll();
+        }
+    }
+}
---
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/ObjectsDeactivatorScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public class ObjectsDeactivatorScript : BaseSceneLoadedLogic
+    {
+        public override void ExecuteLogic(GameObject obj)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
