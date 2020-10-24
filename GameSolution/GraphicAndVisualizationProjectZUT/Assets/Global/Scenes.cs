73b015ee6a401d1075a69844f6c4ce8ce4b4dd36
jacob273 <jakubgmur@op.pl>
2020-10-24 20:51:29 +0200

    added DefaultSceneLoader and Scenes strings

---
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/Global/Scenes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Global
+{
+    public static class Scenes
+    {
+        public const string MainApplicationViewScene = "MainApplicationView";
+        public const string UnityAdventureGameScene = "UnityAdventureGameScene";
+    }
+}
---
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/DefaultSceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Global;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public class DefaultSceneLoader : MonoBehaviour
+    {
+
+        void Start()
+        {
+            int c = SceneManager.sceneCount;
+            for (int i = 0; i < c; i++)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+                print(scene.name);
+                if (scene.name != Scenes.MainApplicationViewScene)
+                {
+                    SceneManager.UnloadSceneAsync(scene);
+                }
+            }
+        }
+
+    }
+}
