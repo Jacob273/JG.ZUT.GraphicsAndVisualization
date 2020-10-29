51286c7538aed3da18888edbfee890464d915aab
jacob273 <jakubgmur@op.pl>
2020-10-29 22:25:28 +0100

    added OnOffAnimationScript for "F" key. It is required to add prefab with animation which will be instantiated

---
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/OnOffAnimationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.JakubGmur.Scripts
+{
+    public class OnOffAnimationScript : MonoBehaviour
+    {
+        public GameObject prefabWithAnimation;
+        private GameObject instantiatedPrefab;
+
+        private static int counter = 1;
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                counter++;
+                if(counter % 2 ==0)
+                {
+                    StartCoroutine(DeadAnimation());
+                }
+                else
+                {
+                    Destroy(instantiatedPrefab);
+                }
+            }
+        }
+
+
+        IEnumerator DeadAnimation()
+        {
+            yield return new WaitForSeconds(1);
+            var currObject = this.gameObject;
+            instantiatedPrefab = Instantiate(prefabWithAnimation, currObject.transform.position, currObject.transform.rotation);
+            instantiatedPrefab.transform.parent = this.gameObject.transform;
+            instantiatedPrefab.transform.Translate(Vector3.up);
+        }
+    }
+}
