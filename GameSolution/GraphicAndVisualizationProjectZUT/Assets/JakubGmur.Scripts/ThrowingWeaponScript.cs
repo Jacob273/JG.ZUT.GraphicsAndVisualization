71d7cc3763b0ee551cb2c474d4a3f0ed106a0125
jacob273 <jakubgmur@op.pl>
2020-11-02 00:25:42 +0100

    ThrowingWeaponScript: shooting only in 1 direction

--- GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/ThrowingWeaponScript.cs
+++ GameSolution/GraphicAndVisualizationProjectZUT/Assets/JakubGmur.Scripts/ThrowingWeaponScript.cs
@@ -11,12 +11,11 @@ namespace Assets.JakubGmur.Scripts
         private static int counter = 1;
         private const float TimeAfterInstantiatedWeaponIsDestroyed = 5.0f;
         private const float TimeDelayBetweenThrowing = 0.5f;
-        
+
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.R))
             {
-                Debug.Log($"Throwing {this.weaponToBeThrown.name}");
                 counter++;
                 if (counter % 2 == 0)
                 {
@@ -31,16 +30,10 @@ namespace Assets.JakubGmur.Scripts
             yield return new WaitForSeconds(TimeDelayBetweenThrowing);
             var currObject = gameObject;
             GameObject instantiatedWeapon = Instantiate(weaponToBeThrown, currObject.transform.position, currObject.transform.rotation);
-
             instantiatedWeapon.GetComponent<WeaponDetails>().SourceId = GetComponent<PlayerObject>().Id;
-            var newPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-            instantiatedWeapon.transform.position = newPosition;
-            instantiatedWeapon.transform.position += Vector3.up;
+
             var rigidBody = instantiatedWeapon.GetComponent<Rigidbody>();
-            var rotation = new Quaternion(instantiatedWeapon.transform.rotation.x, instantiatedWeapon.transform.rotation.y, instantiatedWeapon.transform.rotation.z, instantiatedWeapon.transform.rotation.w);
-            rigidBody.rotation = rotation;
-            rigidBody.AddForce(new Vector3(x: 1, y: 1, z: 0) * throwableDistance);
-            
+            rigidBody.AddForce(new Vector3(x: 1, y: 1, z: 1) * throwableDistance);
             yield return new WaitForSeconds(TimeAfterInstantiatedWeaponIsDestroyed);
             Destroy(instantiatedWeapon);
         }
